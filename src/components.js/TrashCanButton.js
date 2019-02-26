a761983862b77a10756525b5c80ee137f9c4db2f
Delaney Hanson <delaney.hanson@techtonic.com>
2019-02-26 16:21:42 -0700

    changes css and adds button components

---
+++ src/components.js/TrashCanButton.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TrashCanButton extends Component {
+  render() {
+    return (
+      <div>
+        <button className='trash-can fas fa-trash'
+      </div>
+    )
+  }
+}
