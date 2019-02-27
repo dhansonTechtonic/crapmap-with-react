16d504c28ae4f2fef75eb51140e42a4d5f0a57aa
Delaney Hanson <delaney.hanson@techtonic.com>
2019-02-27 14:55:41 -0700

    starts over ish on viewPinModal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ViewPinModal from './components/ViewPinModal';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBox } from '@fortawesome/free-solid-svg-icons'
+import { faCouch } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBox)
+library.add(faCouch)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ViewPinModal />
       </div>
     );
   }
