d50ff0d770a569d83836b68c9ffb84964f7acaed
wwilsonTechtonic <william.wilson@techtonic.com>
2019-02-27 11:09:20 -0700

    Create MyListingsModul component

---
+++ src/components/MyListingsModul.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class MyListingsModul extends Component {
+  render() {
+    return (
+        <div className='vertical-modal'>  
+            <div className="page-header-title">
+                <h1 className="header">
+                    MY LISTINGS
+                </h1>
+            </div>
+            <hr/>
+            <div className="items-listing-container">
+            </div>
+        </div>
+    )
+  }
+}
