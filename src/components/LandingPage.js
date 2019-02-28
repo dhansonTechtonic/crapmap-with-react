bd4e87df4cf088cecc22d1ff18a727b35268bc12
Joshua "Moose" Archer <joshua.archer@techtonic.com>
2019-02-28 08:59:43 -0700

    removed app.js and app.css

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react'
-import CrapMapLogo from './crapmapLogoWhite.png'
-
+import React, { Component } from 'react';
+import BackgroundComponent from './BackgroundComponent';
+import LogoComponent from './LogoComponent';
+import GoogleComponent from './GoogleComponent';
+import FaceBookComponent from './FaceBookComponent';
+import "./LandingPage.css";
 export default class LandingPage extends Component {
   constructor(props) {
     super(props);
@@ -13,19 +16,23 @@ export default class LandingPage extends Component {
     }
   }
 
-  CrapMapLogo() {
-    // return <img src={CrapMapLogo} />
-  }
-
   render() {
     return (
-      <div className="landingPageContainer">
+      <div className="landingPage">
         <div className="landingPageGradient">
-        <img className="emojiBackground" id="landingPageLogoContainer"/>
-          <div ></div>
-          <img src={CrapMapLogo} className="crapMapLogo"/>
+          <BackgroundComponent />
 
-        
+          <div className="landingPageLogoContainer" > 
+            <LogoComponent />
+          </div>
+
+          < div className="googleComponentStyle">
+            <GoogleComponent />
+          </div>
+
+          <div className="faceBookComponentStyle">
+            <FaceBookComponent />
+          </div>
 
         </div>
       </div>
@@ -33,119 +40,5 @@ export default class LandingPage extends Component {
     )
   }
 }
-{/* < div class = "landing-page-gradient" > */}
-  {/* <
-  img src = "images/crapmap-landing-page.png"
-    class = "emoji-background" >
-  <
-  div id = "landing-page-logo-container"
-class = "row"
-style = "text-align: center" >
-  <
-  img src = "images/crapmap-logo-white.png"
-alt = "CrapMap Logo" >
-  <
-  /div>
-
-  <
-  section id = "google-signup" >
-
-  <
-  div >
-  <
-  form action = "/action_page.php" >
-
-  <
-  div class = "row"
-style = "text-align: center" >
-  <!-- <h2 class="fontsize">Login with Google or Manually</h2> -->
-  <
-  a href = "#"
-class = "google btn" >
-  <
-  i class = "fab fa-google" > < /i> Google+ <
-  /a>
-
-  <
-  /div> <
-  /form> <
-  /div>
-
-  <
-  /section>
-
-  <!-- <section id="login-container">
-
-  <
-  div class = "landing-page-button-container" >
-  <
-  button onclick = "document.getElementById('id01').style.display='block'"
-style = "width:auto;" > Login < /button> <
-  /div>
-
-  <
-  div id = "id01"
-class = "modal" >
-
-  <
-  form class = "modal-content animate"
-action = "/action_page.php" >
-
-  <
-  div class = "imgcontainer" >
-  <
-  span onclick = "document.getElementById('id01').style.display='none'"
-class = "close"
-title = "Close Modal" > & times; < /span> <
-img src = "img_avatar2.png"
-alt = "Avatar"
-class = "avatar" >
-  <
-  /div>
-
-  <
-  div class = "container"
-style = "background-color:#2E2D31; display: flex; flex-direction: column; align-items: center;" >
-  <
-  label
-for = "uname" > < b > < /b></label >
-  <
-  input type = "text"
-placeholder = "Enter Username"
-name = "uname"
-required >
-
-  <
-  label
-for = "psw" > < b > < /b></label >
-  <
-  input type = "password"
-placeholder = "Enter Password"
-name = "psw"
-required >
-
-  <
-  button type = "submit" > Login < /button> <
-  /div>
-
-  <
-  div class = "container"
-style = "background-color:#2E2D31; display: flex; flex-direction: column; align-items: center; border-radius: 25px;" >
-  <
-  button type = "button"
-onclick = "document.getElementById('id01').style.display='none'"
-class = "cancelbtn" >
-  Cancel <
-  /button> <
-  span class = "psw" > Forgot < a href = "#" > password ? < /a></span >
-  <
-  /div>
 
 
-  <
-  /form> <
-  /div>
-
-  <
-  /section> --> <
-  /div> */}
