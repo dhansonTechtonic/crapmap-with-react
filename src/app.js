5af7d6c61218ac654dcb88643ee5e8ddadab72bf
Michael Eckstine <michael.eckstine@techtonic.com>
2019-02-28 10:56:39 -0700

    renamed app.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react'
 
-// components import
-import LandingPage from './components/landingPage/LandingPage'
-import AddPinModal from './components/addEditPin/AddPinModal'
-import EditPinModal from './components/addEditPin/EditPinModal'
-
 export default class App extends Component {
   render() {
     return (
       <div>
-        <LandingPage />
-        <AddPinModal />
-        <EditPinModal />
+        
       </div>
     )
   }
