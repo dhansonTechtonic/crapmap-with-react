9afd1c90055526c5a904230cb0a30dc090cff437
wwilsonTechtonic <william.wilson@techtonic.com>
2019-03-01 13:07:32 -0700

    Redo login component

--- src/components/landingPage/FaceBookComponent.js
+++ src/components/landingPage/FaceBookComponent.js
@@ -1,32 +1,73 @@
 import React, { Component } from 'react';
-// import FacebookLogin from 'react-facebook-login';
-import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
+import firebase, { auth, provider} from './../../firebase.js';
+// mport FacebookLogin from 'react-facebook-login';
+//import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
 
 import './Facebook.css'
 
 export default class FacebookComponent extends Component {
 
-  responseFacebook(response) {
-    console.log(response);
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      displayName: '',
+      email: '',
+      photoURL: '',
+      uid: 0
+    }
+    this.provider = new firebase.auth.FacebookAuthProvider()
+    this.login = this.login.bind(this); 
+    this.logout = this.logout.bind(this);
+  }
+
+  login() {
+    auth.signInWithPopup(this.provider) 
+      .then((result) => {
+        const user = result.user;
+        this.setState({
+          user: !!user,
+          displayName: user.displayName,
+          email: user.email,
+          uid: user.uid
+        });
+        this.props.sendData(this.props.val);
+      });
+  }
+
+  logout() {
+    auth.signOut()
+      .then(() => {
+        this.setState({
+          user: null,
+          displayName: '',
+          email: '',
+          photoURL: '',
+          uid: 0
+        });
+      });
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ 
+          user: !!user,
+          displayName: user.displayName,
+          email: user.email,
+          uid: user.uid 
+        });
+      } 
+    });
   }
 
   render() {
-    const {responseFacebook, } = this.props;
-
     return (
-      <FacebookLogin
-        appId = "1088597931155576"
-        autoLoad={true}
-        fields = "name,email,picture"
-        callback={responseFacebook}
-        render={renderProps => (
-          <button className="faceBookComponentStyle" 
-          onClick={renderProps.onClick}>
-          <span icon="fa-facebook"></span>
-            Login with Facebook
-          </button>
-        )}
-      />
+      <button className="faceBookComponentStyle" 
+      onClick={this.login} sendData={this.state.user}>
+      <span icon="fa-facebook"></span>
+        Login with Facebook
+      </button>
     ) 
   }
 }
--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -1,43 +1,63 @@
 import React, { Component } from 'react';
+import firebase, {auth} from './../../firebase.js';
 import BackgroundComponent from './BackgroundComponent';
 import LogoComponent from './LogoComponent';
-import GoogleComponent from './GoogleComponent';
-import FaceBookComponent from './FaceBookComponent';
+import LoginComponent from './LoginComponent';
 import "./LandingPage.css";
 
 export default class LandingPage extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-    username: '',
     user: null
-    }
+    };
+  this.logOut = this.logOut.bind(this);
   }
 
 
+  userLogin = (userData) =>{
+    this.setState({
+      user: userData,
+    })
+  }
 
-  
+  logOut() {
+    auth.signOut()
+      .then(() => {
+        this.setState({
+          user: null,
+          displayName: '',
+          email: '',
+          uid: 0
+        });
+      });
+  }
 
   render() {
     return (
+      <div>
+      {!this.state.user ?
+        <div className="landingPage">
+          <div className="landingPageGradient">
+            <BackgroundComponent />
+            <div className="landingPageLogoContainer" > 
+              <LogoComponent />
+            </div>
+              <LoginComponent sendData={this.userLogin} provider={new firebase.auth.GoogleAuthProvider()} providerName={`Google`}/>                
+          </div>
+        </div>
+      :
       <div className="landingPage">
         <div className="landingPageGradient">
           <BackgroundComponent />
-
           <div className="landingPageLogoContainer" > 
             <LogoComponent />
           </div>
-
-          < div className="googleComponentStyle">
-            <GoogleComponent />
-          </div>
-
-          {/* <div className="faceBookComponentStyle">
-            <FaceBookComponent />
-          </div> */}
-
+            <button className='google' onClick={this.logOut}>Log Out</button> 
         </div>
+      </div>
+      
+      }
       </div>
       
     )
