4f8bedfceabbfafc76a8eced721878c027beba0f
Delaney Hanson <delaney.hanson@techtonic.com>
2019-03-04 10:24:43 -0700

    renders AddPinModal

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-
+import AddPinModal from './components/addEditPin/AddPinModal'
 export default class App extends Component {
   render() {
     return (
       <div>
-        
+        <AddPinModal />
       </div>
     )
   }
