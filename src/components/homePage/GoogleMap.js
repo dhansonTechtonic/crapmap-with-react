c983246f77e38f037666a84c7050796c79b7cea0
Hannah Loftus <hannahloftus@TechtonicsMBP32.localdomain>
2019-03-04 09:25:36 -0700

    google maps with api

--- src/components/homePage/GoogleMap.js
+++ src/components/homePage/GoogleMap.js
@@ -1,37 +1,38 @@
 import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+// import {withScriptjs, withGoogleMap, GoogleMap, Marker} from 'react-google-maps';
 import { isAbsolute } from 'path';
-// import CRAP_MAP_API_KEY from '.env';
-// const CRAP_MAP_API_KEY = process.env.CRAP_MAP_API_KEY;
+// const mapStyles = require('./GoogleMapStyles.json')
 
 
-const mapStyles = {
+const mapStylesDefaults = {
   position: isAbsolute,  
   width: '100%',
-  height: '100%'
+  height: '100%',
 };
 
-
-
 export class MapContainer extends Component {
+
   render() {
     return (
         <div className='map-container'>
       <Map
+      
         google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-         lat: 105.2705,
-         lng: 40.0150
-        }}        
+        zoom={15}
+        style={mapStylesDefaults}
+        centerAroundCurrentLocation={true}
+        draggable={true} 
+        
       />
+    
 </div>
     );
   }
 }
 
 export default GoogleApiWrapper({
-  // apiKey: CRAP_MAP_API_KEY
+  apiKey:process.env.REACT_APP_CRAP_MAP_API_KEY
 })(MapContainer);
 
+  
