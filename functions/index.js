f77c45cd563c6eb3a3e0ce65788c0689d417d3ed
wwilsonTechtonic <william.wilson@techtonic.com>
2019-03-11 16:01:09 -0600

    Add basic get endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 const cors = require("cors");
-const admin = require("firebase-admin");
-const express = require("express");
-admin.initializeApp(functions.config().firebase); //importing firebase-functions in which case you dont need to service account json at all
-
+const admin = require('firebase-admin');
+const express = require('express');
+admin.initializeApp(functions.config().firebase); //importing firebase-functions in which case you don't need to service account JSON at all
+  
 
 const listings = express();
 let db = admin.firestore();
@@ -13,8 +13,8 @@ listings.get("/myListings", (request, response) =>{
     let itemsRef = db.collection('listings');
     let query = itemsRef.get().then(function(querySnapshot){
         if(querySnapshot){
-            console.log(querySnapshot);
-            response.send(querySnapshot);
+            console.log(querySnapshot.docs);
+            response.send(querySnapshot.docs.json());
         }else{
             response.send("Collection does not exist");
         }
@@ -22,9 +22,16 @@ listings.get("/myListings", (request, response) =>{
     }).catch (err => err)
 });
 
+const apiListings = functions.https.onRequest((request, response) => {
+    if (!request.path) {
+      request.url = `/${request.url}` // prepend '/' to keep query params if any
+    }
+    return listings(request, response)
+  })
+
 
 
 module.exports= {
-    listings
+    apiListings
 }
 
--- src/components/MyListings/MyListingsModal.jsx
+++ src/components/MyListings/MyListingsModal.jsx
@@ -7,8 +7,23 @@ import PropTypes from 'prop-types'
 import MyListingsPost from './MyListingsPost'
 
 export default class MyListingsModal extends Component {
-  render() {
 
+  async _getListings(){
+    return await fetch(`https://us-central1-crapmap-c5c7f.cloudfunctions.net/apiListings/myListings`, {
+
+    }).then((res) =>{
+      return res.json();
+    }).then((data) =>{
+      console.log(data);
+      console.log('test');
+    })
+  }
+
+  componentDidMount(){
+    this._getListings();
+  }
+
+  render() {
     if (!this.props.show) {
       return null;
     }
