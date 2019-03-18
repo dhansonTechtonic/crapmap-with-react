277022933f15f0c829502049ef4321f9bbdbbfca
Michael Eckstine <michael.eckstine@techtonic.com>
2019-03-18 14:38:13 -0600

    update and delete pin + refactor

--- src/redux/reducers/pinReducer.js
+++ src/redux/reducers/pinReducer.js
@@ -3,37 +3,16 @@
  const initPin = [];
  
  export default async function pinReducer(state=initPin, action){
-    console.log(action) 
+    // console.log(action) 
     let payload = action.payload;
      switch (action.type) {
         case NEW_PIN: {
-                let pinState = state;
-                fetch('https://us-central1-crapmap-c5c7f.cloudfunctions.net/api/pins/new', {
-                    method: 'POST',
-                    body: payload
-                })
-                .then(res => {
-                    console.log('Success:', res);
-                    Object.assign({}, state, {
-                        pins: pinState.push(payload)
-                    });
-                })
-                .catch(err => console.error('Error:', err))
+                // let pinState = state;
+                console.log('new pin reducer')
                 break;
             }
         case UPDATE_PIN: {
-                fetch('https://us-central1-crapmap-c5c7f.cloudfunctions.net/api/pins/update', {
-                    method: 'PUT',
-                    body: payload
-                })
-                .then(res => {
-                    console.log("Success:", res);
-                    Object.assign({}, state, {
-                        // select original and update to payload
-                        // original: payload
-                    });
-                })
-                .catch(err => console.error('Error:', err))
+                console.log('update pin reducer ')
                 break;
             }
         case GET_PINS_FULFILLED: {
@@ -44,15 +23,7 @@
                 }
             }
         case DELETE_PIN: {
-                fetch('https://us-central1-crapmap-c5c7f.cloudfunctions.net/api/pins/delete', {
-                    method: 'DELETE',
-                    body: payload
-                })
-                .then(res => {
-                    console.log('Success:', res);
-                    //Object method to remove 
-                })
-                .catch(err => console.error('Error:', err))
+                console.log('inside delete reducer')
                 break;
              }
         default: return state;
