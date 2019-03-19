2bb226e6ba79584cfcec301ffb52ff6815fa83ae
Michael Eckstine <michael.eckstine@techtonic.com>
2019-03-19 14:08:56 -0600

    refactor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import {BrowserRouter} from 'react-router-dom'
 
 import store from "./redux/store"
 import {Provider} from 'react-redux'
@@ -10,14 +9,10 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import A2p from './A2p.js';
 
-
 ReactDOM.render(
         <Provider store={store}>
                 <A2p />
         </Provider>
 , document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
