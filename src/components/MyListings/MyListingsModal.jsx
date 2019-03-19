d694d75ab522d93f4140f0d0066d722a906d7cf8
Michael Eckstine <michael.eckstine@techtonic.com>
2019-03-19 10:20:10 -0600

    myPinsModal populating from db

--- src/components/MyListings/MyListingsModal.jsx
+++ src/components/MyListings/MyListingsModal.jsx
@@ -3,51 +3,72 @@ import '../App.css'
 import ExitButton from '../buttons/ExitButton'
 
 import PropTypes from 'prop-types'
+import {connect} from 'react-redux';
 
-import MyListingsPost from './MyListingsPost'
+
 import { IconButton } from '@material-ui/core';
 
-export default class MyListingsModal extends Component {
-
-  async _getListings(){
-    return await fetch(`https://us-central1-crapmap-c5c7f.cloudfunctions.net/apiListings/myListings`, {
-
-    }).then((res) =>{
-      return res.json();
-    }).then((data) =>{
-      console.log(data);
-      console.log('test');
-    })
+class MyListingsModal extends Component {
+  constructor(){
+    super();
+    this.state = {
+      user: {},
+      pins: []
+    }
   }
 
-  // componentDidMount(){
-  //   this._getListings();
-  // }
+  componentDidUpdate(prevProps) {
+    
+    // reduces pins promise to just pin array on update
+    if (this.props.pins !== prevProps.pins) {
+      this.props.pins.then((val) => { this.setState({ pins: val.pins }) })
+    }
+  }
 
   render() {
     if (!this.props.show) {
       return null;
     }
 
+    let posts = this.state.pins.map((pin) => {
+
+      // each post needs key (unique property)
+      // also need to ensure on the front end that no posts get submitted with a single empty field.
+
+      return <div className='pin-card'>
+        <span className='my-pin-image'></span>
+        <p className='my-pin-title'>{pin._fieldsProto.title.stringValue}</p>
+        <p className='my-pin-category'>{pin._fieldsProto.category.stringValue}</p>
+      </div>
+    })
+
+
     return (
         <div className='vertical-modal'>  
             <div className="header">
-                <h2 >My Listings</h2>
+                <h2 >My Pins</h2>
             <IconButton onClick={this.props.onClose}>
             <ExitButton />
             </IconButton>
             </div>
             <hr/>
             <div className="items-listing-container">
-              <MyListingsPost />
+              {posts}
             </div>
         </div>
-    )
-  }
-}
+)}}
 
 MyListingsModal.propTypes = {
   onClose: PropTypes.func.isRequired,
   show: PropTypes.bool,
   children: PropTypes.node
 }
+
+function mapStateToProps(reduxState) {
+  return {
+    user: reduxState.user,
+    pins: reduxState.pins
+  }
+}
+
+export default connect(mapStateToProps)(MyListingsModal);
