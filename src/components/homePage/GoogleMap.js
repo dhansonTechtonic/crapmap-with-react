2dbb55deaf2630b34b820cde95f3e235432d2c6c
Hannah Loftus <hannahloftus@TechtonicsMBP32.localdomain>
2019-03-19 11:13:15 -0600

    working store

--- src/components/homePage/GoogleMap.js
+++ src/components/homePage/GoogleMap.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 import { isAbsolute } from 'path';
-// import store from '../../redux/store'
-// import {getPins} from '../../redux/actions/pinActions'
-
-// import PropTypes from 'prop-types'
+import store from '../../redux/store'
 import ViewPinModal from './ViewPinModal';
 
 const mapStylesDefaults = {
@@ -12,21 +9,6 @@ const mapStylesDefaults = {
   width: '100%',
   height: '100%',
 };
-
-// const pinsArray = [
-//   {
-//     lat: 40.021226,
-//     lng: -105.218359
-//   },
-//   {
-//     lat: 40.221226,
-//     lng: -105.228359
-//   },
-//   {
-//     lat: 40.121226,
-//     lng: -105.228359
-//   }
-// ]
 export class MapContainer extends Component {
 
   constructor(props) {
@@ -37,71 +19,44 @@ export class MapContainer extends Component {
     this.toggleViewPinModal = this.toggleViewPinModal.bind(this)
   }
 
-//update with mapstate to props 
-
   toggleViewPinModal = () => {
     this.setState({
       viewPinModalIsOpen: !this.state.viewPinModalIsOpen,
     });
   }
 
-  init () {
-    console.log("this is load")
-    // console.log(store.getState())
+  componentDidUpdate(prevProps){ //on update...
+    let response = store.getState().pins
+    response.then(
+      (response) => {
+     let pinsArray = response.pins
+    let viewPinsArray = [];
+    for (let i =0; i<=pinsArray.length;i++) {
+    
+    let locationObj ={
+      lat: pinsArray[i]._fieldsProto.location.mapValue.fields.lat.doubleValue, 
+      lng: pinsArray[i]._fieldsProto.location.mapValue.fields.lng.doubleValue
+    }
+    // console.log(locationObj);
+    viewPinsArray.push(locationObj)
+    // this.fireUpdatePinsLocation(viewPinsArray);
+  }
+  console.log(viewPinsArray, "array")
+  console.log(viewPinsArray.length)
+
+  return viewPinsArray
+
+    }
+    )
   }
 
-
-  // componentDidMount() {
-  //   createMarkers();
-
+  // fireUpdatePinsLocation(data){
+  //   let ltlng = maps.LatLng(data)
+  //   console.log(ltlng)
   // }
-
-  firePlacePin (pinResponse) {
-    // console.log("inside firePlacePin", pinResponse)
-    const locationObj = {
-
-      // lat : pinResponse[2]._fieldsProto.location.mapValue.fields.lat.doubleValue,
-      lat : 40.021226,
-      // lng : pinResponse[2]._fieldsProto.location.mapValue.fields.lng.doubleValue
-      lng : -105.218359
-    };
-    console.log(locationObj)
-    return locationObj;
-  }
-
-// createMarkers() {
-//   const pinsArray = [
-//     {
-//       lat: 40.021226,
-//       lng: -105.218359
-//     },
-//     {
-//       lat: 40.221226,
-//       lng: -105.228359
-//     },
-//     {
-//       lat: 40.121226,
-//       lng: -105.228359
-//     }
-//   ]
-
-//   return navLinks.map((b, i) => {
-//     console.log(b.long)
-//     return new google.maps.Marker({
-//       position: new google.maps.LatLng(b.lat, b.long),
-//       map: this.map
-//     })
-//   })
-
-
-  // }
-
 
 
  render() {
-
-  // store.dispatch(getPins({}));
-  data = this.props.pins
 
     return (
         <div className='map-container'>
@@ -113,15 +68,16 @@ export class MapContainer extends Component {
     centerAroundCurrentLocation={true}
     draggable={true} 
 >
-{data.map(item => (
-  <Marker ref={this.onMarkerMounted}
-    key={item.id}
-    title={item.name}
-    name={item.name}
-    position={{ lat: item.lat, lng: item.lng }}
-  />
-))}
 
+
+
+ {/* <Marker onClick={this.toggleViewPinModal}
+      position={this.props.markerLocation}  
+     
+     />  */}
+
+ {/* <Marker onClick={this.toggleViewPinModal}
+    position={} /> */}
 </Map>
 
 <div className="view-pin-container">
@@ -129,8 +85,6 @@ export class MapContainer extends Component {
 </div>
 
 </div>
-
-
 
     );
   }
