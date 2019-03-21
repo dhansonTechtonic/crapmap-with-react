4b39a7612d4bfd6c2d95ffcc219886bb0edd2903
Michael Eckstine <michael.eckstine@techtonic.com>
2019-03-21 14:03:08 -0600

    myCrap modal

--- src/components/MyListings/MyListingsPost.jsx
+++ src/components/MyListings/MyListingsPost.jsx
@@ -21,28 +21,22 @@ const styles = {
 };
 
 //let posts = this.state.pins.map((pin) => {
-
-      // each post needs key (unique property)
-      // also need to ensure on the front end that no posts get submitted with a single empty field.
-
-     // console.log(pin);
-
     //   return <div className='pin-card'>
     //     <span className='my-pin-image'></span>
     //     <p className='my-pin-title'>{pin._fieldsProto.title.stringValue}</p>
     //     <p className='my-pin-category'>{pin._fieldsProto.category.stringValue}</p>
     //     <p className='my-pin-category'>{pin._ref._path.segments[1]}</p>
-
     //     <button className='my-pin-delete' onClick={() => {
     //       this.handleDeletePin(pin._ref._path.segments[1])
     //     }}></button>
     //   </div>
     // })
-        
+
+
 function MyListingsPost(props){
-  const { classes } = props;
+
+    const { classes } = props;
        
-  // need endpoint for get all pins by userID
 
   // let pinArray = store.dispatch(()) 
   // or
@@ -53,29 +47,32 @@ function MyListingsPost(props){
   //   store.dispatch(deletePin(input));
   // }
   return (
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={image}
-          title="My ipsum crap."
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            My ipsum crap.
+
+    props.userPins.map((pin) => 
+      <Card className={classes.card}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={image}
+            title={'nothing'}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {pin._fieldsProto.title.stringValue}
         </Typography>
-          <Typography component="p">
-            At this place in this town, CO 80021
+            <Typography component="p">
+              At this place in this town, CO 80021
         </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-      <EditPinModal />
-      {/* <Button color="error" style={{ left: 110, display: 'block' }} onClick={() => {
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <EditPinModal />
+          {/* <Button color="error" style={{ left: 110, display: 'block' }} onClick={() => {
           this.handleDeletePin(pin._ref._path.segments[1])
         }}>Delete</Button> */}
-      </CardActions>
-    </Card>
+        </CardActions>
+      </Card>
+    )
   )
 }
 
