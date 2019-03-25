d3ef1e82e7a11d7c6b6da90a8c66831586adba5b
Delaney Hanson <delaney.hanson@techtonic.com>
2019-03-25 11:03:38 -0600

    fixes bottom nav colors

--- src/components/buttons/SortButtons.js
+++ src/components/buttons/SortButtons.js
@@ -31,12 +31,12 @@ class SortButtons extends React.Component {
         return (
             <div className="sort-buttons">
                 <BottomNavigation value={this.state.value} onChange={this.handleChange} className={classes.root}>
-                    <BottomNavigationAction id="bnFurniture" label="Furniture" value="Furniture" style={{ fontSize: '1.5em', color: '#ff4700'}} icon={<FontAwesomeIcon icon='couch' />}/>
-                    <BottomNavigationAction id="bnAuto" label="Auto Parts" value="Auto Parts" style={{ fontSize: '1.5em', color: 'rgb(227, 68, 255)' }} icon={<FontAwesomeIcon icon='car' />}/>
-                    <BottomNavigationAction id="bnSports" label="Sports" value="Sports" style={{ fontSize: '1.5em', color: 'rgb(253, 88, 157)' }} icon={<FontAwesomeIcon icon='baseball-ball' />}/>
-                    <BottomNavigationAction id="bnGadgets" label="Gadgets" value="Gadgets" style={{ fontSize: '1.5em', color: 'rgb(115, 40, 255)' }} icon={<FontAwesomeIcon icon='tv' />}/>
-                    <BottomNavigationAction id="bnMisc" label="Misc" value="Misc" style={{ fontSize: '1.5em', color: 'rgb(71, 141, 255)' }} icon={<FontAwesomeIcon icon='question-circle' />}/>
-                    <BottomNavigationAction id="bnAll" label="All" value="All" style={{ fontSize: '1.5em', color: '#00FFDE'  }} icon={<FontAwesomeIcon icon='infinity' />}/>
+                    <BottomNavigationAction id="catFurniture" label="Furniture" value="Furniture" style={{ fontSize: '1.5em', color: '#ff4700'}} icon={<FontAwesomeIcon icon='couch' />}/>
+                    <BottomNavigationAction id="catAuto" label="Auto Parts" value="Auto Parts" style={{ fontSize: '1.5em', color: 'rgb(253, 88, 157)' }} icon={<FontAwesomeIcon icon='car' />}/>
+                    <BottomNavigationAction id="catSports" label="Sports" value="Sports" style={{ fontSize: '1.5em', color: 'rgb(227, 68, 255)' }} icon={<FontAwesomeIcon icon='baseball-ball' />}/>
+                    <BottomNavigationAction id="catGadgets" label="Gadgets" value="Gadgets" style={{ fontSize: '1.5em', color: 'rgb(115, 40, 255)' }} icon={<FontAwesomeIcon icon='tv' />}/>
+                    <BottomNavigationAction id="catMisc" label="Misc" value="Misc" style={{ fontSize: '1.5em', color: 'rgb(71, 141, 255)' }} icon={<FontAwesomeIcon icon='question-circle' />}/>
+                    <BottomNavigationAction id="catAll" label="All" value="All" style={{ fontSize: '1.5em', color: '#00FFDE'  }} icon={<FontAwesomeIcon icon='infinity' />}/>
                 </BottomNavigation>
             </div>
         );
