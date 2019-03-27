180668c6ec91192484277f67653dc2d9cff6ffc8
Hannah Loftus <hannahloftus@Techtonics-MacBook-Pro.local>
2019-03-27 08:35:52 -0600

    actually working view pin, and async func

--- src/components/homePage/GoogleMap.js
+++ src/components/homePage/GoogleMap.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Marker, In } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 import { isAbsolute } from 'path';
 import {connect} from 'react-redux';
 import styles from './GoogleMapsJSON.json';
 import ViewPinModal from './ViewPinModal';
-
+import firebase from './../../firebase.js'
 
 const mapStylesDefaults = {
   position: isAbsolute,  
@@ -19,15 +19,26 @@ export class MapContainer extends Component {
       viewCardIsOpen: false,
       pins: [],
       pinData: {}, 
+      img: null,
+
       }
       this.toggleViewPinModal = this.toggleViewPinModal.bind(this)
+      this.toggleViewPinModalClose = this.toggleViewPinModalClose.bind(this)
     };
 
-  toggleViewPinModal(e) {
+  async toggleViewPinModal(e) {
     let targetPin = e;
+    let imgURL = await this.handleImageURL(targetPin.img)
     this.setState({
       viewCardIsOpen: !this.state.viewCardIsOpen,
-      pinData: targetPin
+      pinData: targetPin,
+      img: imgURL
+    });
+  }
+
+  toggleViewPinModalClose() {
+    this.setState({
+      viewCardIsOpen: false,
     });
   }
 
@@ -39,9 +50,9 @@ export class MapContainer extends Component {
 
   findColor (category) {
     let icon;
-    // if (!this.state.dibState) {
-    //   return icon = { path: window.google.maps.SymbolPath.CIRCLE, scale: 6, strokeColor: '#6f6d75' }
-    // }
+    if (category === false) {
+      return icon = { path: window.google.maps.SymbolPath.CIRCLE, scale: 6, strokeColor: '#6f6d75' }
+    }
       switch (category) {
         case "Furniture":
           icon = { path: window.google.maps.SymbolPath.CIRCLE, scale: 4, strokeColor: '#ff4700' }
@@ -69,14 +80,19 @@ export class MapContainer extends Component {
       }
       return icon;
   }
-  
-  findZip(zipCode) {
-  }
 
-  // changeIcon (e) {
-  //   console.log(e)
-  //   return e.icon = { path: window.google.maps.SymbolPath.CIRCLE, scale: 6, strokeColor: '#6f6d75' }
-  // }
+  async handleImageURL(imgurl) {
+    
+    let storage = firebase.storage();
+    let storageRef = storage.ref(imgurl);
+    let url = await storageRef.getDownloadURL().then(function(url) {
+      return url
+    }).catch(function(error) {
+      console.log(error);
+      return 'https://firebasestorage.googleapis.com/v0/b/crapmap-c5c7f.appspot.com/o/assets%2FcrapmapLogoWhite.png?alt=media&token=8fcd6ae0-460f-4fb7-9504-866dab987042'
+    });
+    return url
+  }
 
  render() { 
   if (!this.props.loaded) {
@@ -99,17 +115,15 @@ export class MapContainer extends Component {
 
  {this.state.pins.map((pin) => {
   return (
+
   <Marker
     key={pin._ref._path.segments[1]}
     active={this.state.dibState}
-    // pin = {pin}
     name={pin._fieldsProto.title.stringValue}
-
     icon={this.findColor(pin._fieldsProto.category.stringValue)}
-
     category={pin._fieldsProto.category.stringValue}
     itemSize={pin._fieldsProto.size.stringValue}
-    // img={pin._fieldsProto.img.stringValue}
+    img={pin._fieldsProto.img.stringValue}
     position={{ lat:pin._fieldsProto.location.mapValue.fields.lat.doubleValue,
                 lng:pin._fieldsProto.location.mapValue.fields.lng.doubleValue }}
     onClick={this.toggleViewPinModal}
@@ -120,7 +134,7 @@ export class MapContainer extends Component {
 
 </Map>
 
-<ViewPinModal show={this.state.viewCardIsOpen} data={this.state.pinData} onClick={this.toggleViewPinModal} /> 
+<ViewPinModal show={this.state.viewCardIsOpen} data={this.state.pinData} img={this.state.img} onClick={this.toggleViewPinModalClose} handleDibsClick={this.handleDibState} /> 
 
 </div> 
     )
