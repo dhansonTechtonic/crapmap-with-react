7c53e5cff6237065e83d6b61d5d3c342a06e2640
Joshua "Moose" Archer <joshua.archer@techtonic.com>
2019-04-02 13:34:18 -0600

    testing

--- functions/controllers/CommentController.js
+++ functions/controllers/CommentController.js
@@ -8,8 +8,65 @@ let db = admin.firestore();
 const express = require('express');
 const router = express.Router();
 
-router.post('/comment/', (req, res) => {
+const nodemailer = require('nodemailer');
 
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+        user: 'crapmapsupport',
+        pass: 'passw0rd'
+    }
+});
+
+router.post('/testEmail',(req,res)=>{
+    let mailOptions = {
+        from: 'joshua.archer@techtonic.com',
+        to: 'joshua.archer@techtonic.com',
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!'
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.status(500).send(error);
+        } else {
+            res.status(200).send({info: info, msg: 'worked'});
+            console.log('Email sent: ' + info.response);
+        }
+    });
+    
+})
+
+// psuedo code 
+// router.post('/send', (req, res, next) => {
+//     var name = req.body.name
+//     var email = req.body.email
+//     var message = req.body.message
+
+//     var mail = {
+//         from: name,
+//         to: email,  //Change to email address that you want to receive messages on
+//         subject: message,
+//         text: content
+//     }
+
+//     transporter.sendMail(mail, (err, data) => {
+//         if (err) {
+//             res.json({
+//                 msg: 'fail'
+//             })
+//         } else {
+//             res.json({
+//                 msg: `${postCommenter} commented on your post`
+//             })
+//         }
+//     })
+// })
+
+router.post('/comment/', (req, res) => {
+    console.log('in comment endpoint')
             let commentObj = {
                 author: req.body.author,
                 date: new Date(),
@@ -17,11 +74,33 @@ router.post('/comment/', (req, res) => {
                 pinID: req.body.pinID,
                 userID: req.body.userID
             }
+            console.log(commentObj)
 
             let commentsDB = db.collection('comments')
 
             commentsDB.add(commentObj)
-            .then(res => {
+            .then(async (res) => {
+                console.log(res)
+                var pin = await db.collection('pins').doc(req.body.pinID).get();
+                console.log('pin below');
+                console.log(pin);
+                console.log(pin.docs[0].userID);
+                // var postUser = pin.docs[0].userID;
+
+                let mailOptions = {
+                    from: 'joshua.archer@techtonic.com',
+                    to: res.body.author,
+                    subject: 'Sending Email using Node.js',
+                    text: 'That was easy!'
+                };
+
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                    }
+                });
                 res.send("posted")
                 return res;
             })
